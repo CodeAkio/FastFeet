c47e64d069a4cfa73896c2c7e422fc4dc8f80c74
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-02-22 22:02:24 -0300

    Web: Order style

---
+++ web/src/pages/Orders/styles.js
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  max-width: 1200px;
+  margin: 36px auto;
+
+  form {
+    input {
+      height: 36px;
+      padding: 0 15px;
+      color: #999;
+      margin: 0 0 10px;
+      &::placeholder {
+        color: #999;
+      }
+    }
+    button {
+      margin: 5px 0 0;
+      height: 44px;
+      background: #3b9eff;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+      &:hover {
+        background: ${darken(0.03, '#3b9eff')};
+      }
+    }
+    a {
+      color: #fff;
+      margin-top: 15px;
+      font-size: 16px;
+      opacity: 0.8;
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+  > button {
+    width: 100%;
+    margin: 10px 0 0;
+    height: 44px;
+    background: #f64c75;
+    font-weight: bold;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    font-size: 16px;
+    transition: background 0.2s;
+    &:hover {
+      background: ${darken(0.08, '#f64c75')};
+    }
+  }
+`;
+
+export const HeaderDiv = styled.div`
+  width: 100%;
+  display: inline-block;
+  margin-top: 30px;
+
+  > div {
+    position: relative;
+    > input {
+      float: left;
+      height: 36px;
+      width: 236px;
+      background: #fff;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      padding-left: 40px;
+    }
+    > i {
+      position: absolute;
+      left: 16px;
+      top: 10px;
+      color: #999;
+    }
+  }
+
+  > button {
+    float: right;
+    height: 36px;
+    background: #7d40e7;
+    border-radius: 4px;
+    padding: 8px 16px;
+    color: #fff;
+    font-size: 14px;
+    font-weight: bold;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+`;
--- web/src/pages/_layouts/default/styles.js
+++ web/src/pages/_layouts/default/styles.js
@@ -3,4 +3,64 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   height: 100%;
   background: #f5f5f5;
+
+  h1 {
+    font-size: 24px;
+    color: #444;
+    padding: 16px 0;
+  }
+
+  table {
+    width: 100%;
+    border-collapse: separate;
+    border-spacing: 0 20px;
+    tr th {
+      background: none;
+      padding: 14px 26px;
+      text-align: left;
+      font-size: 16px;
+      color: #444;
+    }
+    tr td {
+      padding: 20px 26px;
+      background: #fff;
+      font-size: 16px;
+      color: #666;
+    }
+    td:nth-child(1) {
+      width: 5%;
+      border-top-left-radius: 4px;
+      border-bottom-left-radius: 4px;
+    }
+    td:nth-child(2) {
+      width: 30%;
+    }
+    td:nth-child(3) {
+      width: 50%;
+    }
+    td:nth-child(4) {
+      text-align: center;
+      width: 5%;
+      border-top-right-radius: 4px;
+      border-bottom-right-radius: 4px;
+    }
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      background: #fff;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+    }
+    span {
+      color: #fb6f91;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
+    }
+  }
 `;
