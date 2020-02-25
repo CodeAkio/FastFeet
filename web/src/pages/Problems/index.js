21833bd501710e8a69e2240d7cda47334eaac60d
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-02-25 18:19:26 -0300

    Web: List actions options and modal of problems

--- web/src/pages/Problems/index.js
+++ web/src/pages/Problems/index.js
@@ -1,12 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { MdMoreHoriz } from 'react-icons/md';
+import React, { useRef, useState, useEffect } from 'react';
+import { MdMoreHoriz, MdVisibility, MdDeleteForever } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 import api from '~/services/api';
 import formattedId from '~/utils/formattedId';
-import { Container } from './styles';
+import { Container, OptionsList, Modal, ModalContent } from './styles';
 
 export default function Problems() {
   const [problems, setProblems] = useState([]);
+  const [visibleOption, setVisibleOption] = useState(false);
+  const [visibleModal, setVisibleModal] = useState(false);
+
+  const modalContentRef = useRef();
 
   useEffect(() => {
     async function loadRecepients() {
@@ -17,6 +22,31 @@ export default function Problems() {
 
     loadRecepients();
   }, [problems]);
+
+  const handleOutsideModalClick = e => {
+    if (modalContentRef.current.contains(e.target)) {
+      return;
+    }
+    if (visibleModal) {
+      setVisibleModal(!visibleModal);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideModalClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideModalClick);
+    };
+  });
+
+  function handleToggleVisibleOptions() {
+    setVisibleOption(!visibleOption);
+  }
+
+  function handleToggleVisibleModal() {
+    setVisibleModal(!visibleModal);
+  }
 
   return (
     <Container>
@@ -29,11 +59,33 @@ export default function Problems() {
           <th>Ações</th>
         </tr>
         {problems.map(problem => (
-          <tr>
+          <tr key={problem.id}>
             <td>{formattedId(problem.delivery.id)}</td>
             <td>{problem.description}</td>
             <td>
-              <MdMoreHoriz size={24} color="#C6C6C6" />
+              <button type="button" onClick={handleToggleVisibleOptions}>
+                <MdMoreHoriz size={24} color="#C6C6C6" />
+              </button>
+              <OptionsList visible={visibleOption}>
+                <ul>
+                  <li>
+                    <MdVisibility size={14} color="#8E5BE8" />
+                    <button type="button" onClick={handleToggleVisibleModal}>
+                      Visualizar
+                    </button>
+                    <Modal visible={visibleModal}>
+                      <ModalContent ref={modalContentRef}>
+                        <h4>VISUALIZAR PROBLEMA</h4>
+                        <p>{problem.description}</p>
+                      </ModalContent>
+                    </Modal>
+                  </li>
+                  <li>
+                    <MdDeleteForever size={14} color="#DE3B3B" />
+                    <Link to="/">Cancelar encomenda</Link>
+                  </li>
+                </ul>
+              </OptionsList>
             </td>
           </tr>
         ))}
--- web/src/pages/Problems/styles.js
+++ web/src/pages/Problems/styles.js
@@ -13,8 +13,14 @@ export const Container = styled.div`
       width: 75%;
     }
     td:last-child {
+      position: relative;
       text-align: center;
       width: 5%;
+      > button {
+        border: 0;
+        background: none;
+        position: relative;
+      }
     }
   }
 
@@ -28,7 +34,7 @@ export const Container = styled.div`
         color: #999;
       }
     }
-    button {
+    > button {
       margin: 5px 0 0;
       height: 44px;
       background: #3b9eff;
@@ -66,5 +72,90 @@ export const Container = styled.div`
     &:hover {
       background: ${darken(0.08, '#f64c75')};
     }
+  }
+`;
+
+export const OptionsList = styled.div`
+  position: absolute;
+  width: 200px;
+  left: calc(50% - 100px);
+  top: calc(50% + 26px);
+  background: #fff;
+  border-radius: 4px;
+  padding: 14px 18px;
+  display: ${props => (props.visible ? 'block' : 'none')};
+  border: 1px solid #0000001a;
+  text-align: left;
+  &::before {
+    content: '';
+    position: absolute;
+    left: calc(50% - 7px);
+    top: -10px;
+    width: 0;
+    height: 0;
+    border-left: 7px solid transparent;
+    border-right: 7px solid transparent;
+    border-bottom: 9px solid #0000001a;
+  }
+  &::after {
+    content: '';
+    position: absolute;
+    left: calc(50% - 6px);
+    top: -9px;
+    width: 0;
+    height: 0;
+    border-left: 6px solid transparent;
+    border-right: 6px solid transparent;
+    border-bottom: 9px solid #fff;
+  }
+  ul li {
+    display: flex;
+    align-items: center;
+    padding: 8px 0;
+    > a {
+      color: #999;
+      font-size: 14px;
+      margin-left: 8px;
+    }
+    > button {
+      background: none;
+      border: 0;
+      color: #999;
+      font-size: 14px;
+      margin-left: 8px;
+    }
+  }
+  ul li + li {
+    border-top: 1px solid #eee;
+  }
+`;
+
+export const Modal = styled.div`
+  display: ${props => (props.visible ? 'flex' : 'none')};
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.7);
+`;
+
+export const ModalContent = styled.div`
+  background-color: #fff;
+  margin: auto;
+  padding: 25px;
+  border: 1px solid #00000033;
+  border-radius: 4px;
+  width: 450px;
+  h4 {
+    font-size: 14px;
+    color: #444;
+  }
+  p {
+    font-size: 16px;
+    color: #666;
+    margin-top: 4px;
   }
 `;
