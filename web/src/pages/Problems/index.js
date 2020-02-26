134f04de785a9a389c0675317af8141b16a20a75
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-02-26 01:25:00 -0300

    Web: Fix open individual actions options, modal and alert, but created error in outside click detect on page problems

--- web/src/pages/Problems/index.js
+++ web/src/pages/Problems/index.js
@@ -17,7 +17,7 @@ import {
 
 export default function Problems() {
   const [problems, setProblems] = useState([]);
-  const [visibleOption, setVisibleOption] = useState(false);
+  // const [visibleOptions, setVisibleOptions] = useState(false);
   const [visibleModal, setVisibleModal] = useState(false);
   const [visibleAlert, setVisibleAlert] = useState(false);
 
@@ -26,12 +26,22 @@ export default function Problems() {
 
   async function loadProblems() {
     const response = await api.get('/delivery/problems');
+    setProblems(
+      response.data.map(p => {
+        return {
+          ...p,
+          // visibleModal: false,
+          visibleOptions: false,
+          // visibleAlert: false,
+        };
+      })
+    );
     setProblems(response.data);
   }
 
   useEffect(() => {
     loadProblems();
-  }, [problems]);
+  }, []);
 
   const handleOutsideModalClick = e => {
     if (modalContentRef.current.contains(e.target)) {
@@ -56,17 +66,84 @@ export default function Problems() {
     };
   });
 
-  function handleToggleVisibleOptions() {
-    setVisibleOption(!visibleOption);
+  // function handleToggleVisibleOptions() {
+  //   setVisibleOptions(!visibleOptions);
+  // }
+
+  // function handleToggleVisibleAlert() {
+  //   setVisibleAlert(!visibleAlert);
+  // }
+
+  // function handleToggleVisibleModal() {
+  //   setVisibleModal(!visibleModal);
+  // }
+
+  function handleToggleVisibleOptions(id) {
+    setProblems(
+      problems.map(p => {
+        if (p.id === id) {
+          return { ...p, visibleOptions: !p.visibleOptions };
+        }
+        return { ...p };
+      })
+    );
   }
 
-  function handleToggleVisibleAlert() {
-    setVisibleAlert(!visibleAlert);
+  function handleToggleVisibleAlert(id) {
+    setProblems(
+      problems.map(p => {
+        if (p.id === id) {
+          return { ...p, visibleAlert: !p.visibleAlert };
+        }
+        return { ...p };
+      })
+    );
   }
 
-  function handleToggleVisibleModal() {
-    setVisibleModal(!visibleModal);
+  function handleToggleVisibleModal(id) {
+    setProblems(
+      problems.map(p => {
+        if (p.id === id) {
+          return { ...p, visibleModal: !p.visibleModal };
+        }
+        return { ...p };
+      })
+    );
   }
+
+  // const handleOutsideModalClick = e => {
+  //   const openModal = problems.find(p => p.visibleModal === true);
+  //   const openAlert = problems.find(p => p.visibleAlert === true);
+
+  //   if (
+  //     modalContentRef.current &&
+  //     !modalContentRef.current.contains(e.target)
+  //   ) {
+  //     console.tron.log(openModal);
+  //     return;
+  //   }
+  //   if (alertContentRef.current.contains(e.target)) {
+  //     return;
+  //   }
+
+  //   if (openModal) {
+  //     // console.tron.log(openModal);
+  //     // handleToggleVisibleModal(openModal.id);
+  //   }
+
+  //   if (openAlert) {
+  //     console.tron.log(openAlert);
+  //     // handleToggleVisibleAlert(openAlert.id);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   document.addEventListener('mousedown', handleOutsideModalClick);
+
+  //   return () => {
+  //     document.removeEventListener('mousedown', handleOutsideModalClick);
+  //   };
+  // });
 
   async function cancelOrder(id) {
     try {
@@ -95,17 +172,23 @@ export default function Problems() {
             <td>{formattedId(problem.delivery.id)}</td>
             <td>{problem.description}</td>
             <td>
-              <button type="button" onClick={handleToggleVisibleOptions}>
+              <button
+                type="button"
+                onClick={() => handleToggleVisibleOptions(problem.id)}
+              >
                 <MdMoreHoriz size={24} color="#C6C6C6" />
               </button>
-              <OptionsList visible={visibleOption}>
+              <OptionsList visible={problem.visibleOptions}>
                 <ul>
                   <li>
                     <MdVisibility size={14} color="#8E5BE8" />
-                    <button type="button" onClick={handleToggleVisibleModal}>
+                    <button
+                      type="button"
+                      onClick={() => handleToggleVisibleModal(problem.id)}
+                    >
                       Visualizar
                     </button>
-                    <Modal visible={visibleModal}>
+                    <Modal visible={problem.visibleModal}>
                       <ModalContent ref={modalContentRef}>
                         <h4>VISUALIZAR PROBLEMA</h4>
                         <p>{problem.description}</p>
@@ -114,10 +197,13 @@ export default function Problems() {
                   </li>
                   <li>
                     <MdDeleteForever size={14} color="#DE3B3B" />
-                    <button type="button" onClick={handleToggleVisibleAlert}>
+                    <button
+                      type="button"
+                      onClick={() => handleToggleVisibleAlert(problem.id)}
+                    >
                       Cancelar encomenda
                     </button>
-                    <CancellationAlert visible={visibleAlert}>
+                    <CancellationAlert visible={problem.visibleAlert}>
                       <CancellationAlertContent ref={alertContentRef}>
                         <h3>Atenção!!!</h3>
                         <p>
@@ -134,7 +220,9 @@ export default function Problems() {
                           >
                             Sim
                           </ConfirmButton>
-                          <CancelButton onClick={handleToggleVisibleAlert}>
+                          <CancelButton
+                            onClick={() => handleToggleVisibleAlert(problem.id)}
+                          >
                             Não
                           </CancelButton>
                         </div>
