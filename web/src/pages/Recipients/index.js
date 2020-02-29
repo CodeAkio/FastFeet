ede80f0545b3cfc14bb857cfadcf1c18a0f32fc1
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-02-29 12:40:57 -0300

    Web: Added action options and delete recipient

--- web/src/pages/Recipients/index.js
+++ web/src/pages/Recipients/index.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { MdAdd, MdMoreHoriz } from 'react-icons/md';
+import { MdAdd, MdMoreHoriz, MdDeleteForever } from 'react-icons/md';
+import { toast } from 'react-toastify';
 
 import api from '~/services/api';
 import formattedId from '~/utils/formattedId';
 import { Container, HeaderDiv } from './styles';
+import OptionsList from '~/components/OptionsList';
+import Alert from '~/components/Alert';
 
 export default function Recipients() {
   const [recipients, setRecipients] = useState([]);
@@ -17,11 +20,50 @@ export default function Recipients() {
         },
       });
 
-      setRecipients(response.data);
+      const data = response.data.map(r => ({
+        ...r,
+        optionsOpen: false,
+        alertOpen: false,
+      }));
+
+      setRecipients(data);
     }
 
     loadRecepients();
-  }, [recipients, search]);
+  }, [search]);
+
+  function handleToggleVisibleOptions(id) {
+    setRecipients(
+      recipients.map(r => {
+        if (r.id === id) {
+          return { ...r, optionsOpen: !r.optionsOpen };
+        }
+        return { ...r };
+      })
+    );
+  }
+
+  function handleToggleVisibleAlert(id) {
+    setRecipients(
+      recipients.map(r => {
+        if (r.id === id) {
+          return { ...r, alertOpen: !r.alertOpen };
+        }
+        return { ...r };
+      })
+    );
+  }
+
+  async function deleteRecipient(id) {
+    try {
+      await api.delete(`/recipients/${id}`);
+      toast.success(
+        `O destinatário ${formattedId(id)} foi removido com sucesso`
+      );
+    } catch (err) {
+      toast.error(`Não foi possível remover o destinatário ${formattedId(id)}`);
+    }
+  }
 
   return (
     <Container>
@@ -57,7 +99,38 @@ export default function Recipients() {
             <td>{recipient.name}</td>
             <td>{`${recipient.street}, ${recipient.number}, ${recipient.city} - ${recipient.state}`}</td>
             <td>
-              <MdMoreHoriz size={24} color="#C6C6C6" />
+              <button
+                type="button"
+                onClick={() => handleToggleVisibleOptions(recipient.id)}
+              >
+                <MdMoreHoriz size={24} color="#C6C6C6" />
+              </button>
+              <OptionsList visible={recipient.optionsOpen}>
+                <li>
+                  <MdDeleteForever size={14} color="#DE3B3B" />
+                  <button
+                    type="button"
+                    onClick={() => handleToggleVisibleAlert(recipient.id)}
+                  >
+                    Excluir
+                  </button>
+                  <Alert
+                    visible={recipient.alertOpen}
+                    handler={handleToggleVisibleAlert}
+                    handlerParam={recipient.id}
+                    handlerConfirm={deleteRecipient}
+                    handlerConfirmParam={recipient.id}
+                  >
+                    <p>
+                      Você realmente deseja remover o destinatário{' '}
+                      <strong>
+                        {recipient.name} ({formattedId(recipient.id)})
+                      </strong>
+                      ?
+                    </p>
+                  </Alert>
+                </li>
+              </OptionsList>
             </td>
           </tr>
         ))}
--- web/src/pages/Recipients/styles.js
+++ web/src/pages/Recipients/styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
 
 export const Container = styled.div`
   max-width: 1200px;
@@ -16,58 +15,14 @@ export const Container = styled.div`
       width: 50%;
     }
     td:last-child {
+      position: relative;
       text-align: center;
       width: 5%;
-    }
-  }
-
-  form {
-    input {
-      height: 36px;
-      padding: 0 15px;
-      color: #999;
-      margin: 0 0 10px;
-      &::placeholder {
-        color: #999;
+      > button {
+        border: 0;
+        background: none;
+        position: relative;
       }
-    }
-    button {
-      margin: 5px 0 0;
-      height: 44px;
-      background: #3b9eff;
-      font-weight: bold;
-      color: #fff;
-      border: 0;
-      border-radius: 4px;
-      font-size: 16px;
-      transition: background 0.2s;
-      &:hover {
-        background: ${darken(0.03, '#3b9eff')};
-      }
-    }
-    a {
-      color: #fff;
-      margin-top: 15px;
-      font-size: 16px;
-      opacity: 0.8;
-      &:hover {
-        opacity: 1;
-      }
-    }
-  }
-  > button {
-    width: 100%;
-    margin: 10px 0 0;
-    height: 44px;
-    background: #f64c75;
-    font-weight: bold;
-    color: #fff;
-    border: 0;
-    border-radius: 4px;
-    font-size: 16px;
-    transition: background 0.2s;
-    &:hover {
-      background: ${darken(0.08, '#f64c75')};
     }
   }
 `;
