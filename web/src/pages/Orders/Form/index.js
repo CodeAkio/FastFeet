c831fef9c3711df171a952fd438a65d75e2ea0f9
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-03-04 22:47:04 -0300

    Web: Fix react select handle value

--- web/src/pages/Orders/Form/index.js
+++ web/src/pages/Orders/Form/index.js
@@ -5,10 +5,10 @@ import PropTypes from 'prop-types';
 import { MdKeyboardArrowLeft, MdCheck } from 'react-icons/md';
 import { Form, Input } from '@rocketseat/unform';
 // import * as Yup from 'yup';
+import AsyncSelect from 'react-select/async';
 import { toast } from 'react-toastify';
 
 import api from '~/services/api';
-import Select from './SelectInput';
 import {
   Container,
   HeaderDiv,
@@ -34,6 +34,10 @@ import {
 
 export default function OrderForm({ match, history }) {
   const [order, setOrder] = useState(null);
+  const [defaultRecipient, setDefaultRecipient] = useState(null);
+  const [defaultDeliveryman, setDefaultDeliveryman] = useState(null);
+  const [recipients, setRecipients] = useState({});
+  const [deliverymen, setDeliverymen] = useState({});
 
   const { id } = match.params;
   const formType = id ? 'edit' : 'new';
@@ -43,10 +47,61 @@ export default function OrderForm({ match, history }) {
       const response = await api.get(`/orders/${id}`);
 
       setOrder(response.data);
+
+      const { recipient, deliveryman } = response.data;
+      console.log(deliveryman);
+      setDefaultRecipient({ value: recipient.id, label: recipient.name });
+      setDefaultDeliveryman({ value: deliveryman.id, label: deliveryman.name });
+    }
+
+    async function loadRecipients() {
+      const response = await api.get('/recipients');
+
+      const data = response.data.map(r => ({
+        value: r.id,
+        label: r.name,
+      }));
+
+      setRecipients(data);
+    }
+
+    async function loadDeliverymen() {
+      const response = await api.get('/deliverymen');
+
+      const data = response.data.map(d => ({
+        value: d.id,
+        label: d.name,
+      }));
+
+      setDeliverymen(data);
     }
 
     loadOrder();
+    loadRecipients();
+    loadDeliverymen();
   }, [id]);
+
+  function filterRecipients(inputValue) {
+    return recipients.filter(e =>
+      e.label.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  }
+
+  function filterDeliverymen(inputValue) {
+    return deliverymen.filter(e =>
+      e.label.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  }
+
+  const promiseRecipients = inputValue =>
+    new Promise(resolve => {
+      resolve(filterRecipients(inputValue));
+    });
+
+  const promiseDeliverymen = inputValue =>
+    new Promise(resolve => {
+      resolve(filterDeliverymen(inputValue));
+    });
 
   async function createOrder(data) {
     try {
@@ -106,15 +161,23 @@ export default function OrderForm({ match, history }) {
         <FormContent>
           <RowFields>
             <SelectContainer size={411} rightSpace>
-              <Select
-                apiPath="recipients"
-                initialValue={order && order.recipient.id}
+              <AsyncSelect
+                cacheOptions
+                defaultOptions={recipients}
+                loadOptions={promiseRecipients}
+                options={recipients}
+                value={defaultRecipient}
+                onChange={setDefaultRecipient}
               />
             </SelectContainer>
             <SelectContainer size={411}>
-              <Select
-                apiPath="deliverymen"
-                initialValue={order && order.deliveryman.id}
+              <AsyncSelect
+                cacheOptions
+                defaultOptions={deliverymen}
+                loadOptions={promiseDeliverymen}
+                options={deliverymen}
+                value={defaultDeliveryman}
+                onChange={setDefaultDeliveryman}
               />
             </SelectContainer>
           </RowFields>
