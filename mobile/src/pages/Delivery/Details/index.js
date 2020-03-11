a7cbf4ee52bd11bf8d681fa39597efca4a4311d0
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-03-11 17:03:35 -0300

    Mobile: Created details page

--- mobile/src/pages/Delivery/Details/index.js
+++ mobile/src/pages/Delivery/Details/index.js
@@ -1,8 +1,112 @@
 import React from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
-// import { Container } from './styles';
+import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function Details() {
-  return <View />;
+import formatDate from '~/utils/formatDate';
+import Background from '~/components/Background';
+import Container from '~/components/Container';
+import {
+  Card,
+  CardHeader,
+  CardHeaderIcon,
+  CardHeaderTitle,
+  Data,
+  DataRow,
+  DataTitle,
+  DataText,
+  CardAction,
+  ActionCenter,
+  ActionButton,
+  ActionText,
+} from './styles';
+
+export default function Details({ navigation }) {
+  const delivery = navigation.getParam('delivery');
+  const { recipient } = delivery;
+
+  return (
+    <Background>
+      <Container>
+        <Card>
+          <CardHeader>
+            <CardHeaderIcon name="local-shipping" />
+            <CardHeaderTitle>Informações da entrega</CardHeaderTitle>
+          </CardHeader>
+          <Data>
+            <DataTitle>DESTINATÁRIO</DataTitle>
+            <DataText>{recipient.name}</DataText>
+          </Data>
+          <Data>
+            <DataTitle>ENDEREÇO DE ENTREGA</DataTitle>
+            <DataText>{`${recipient.street}, ${recipient.number}, ${recipient.city} - ${recipient.state}, ${recipient.cep}`}</DataText>
+          </Data>
+          <Data last>
+            <DataTitle>PRODUTO</DataTitle>
+            <DataText>{delivery.product}</DataText>
+          </Data>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardHeaderIcon name="event" />
+            <CardHeaderTitle>Situação da entrega</CardHeaderTitle>
+          </CardHeader>
+          <Data>
+            <DataTitle>STATUS</DataTitle>
+            <DataText>{delivery.status}</DataText>
+          </Data>
+          <DataRow>
+            <Data last>
+              <DataTitle>DATA DE RETIRADA</DataTitle>
+              <DataText>{formatDate(delivery.start_date)}</DataText>
+            </Data>
+            <Data last>
+              <DataTitle>DATA DE ENTREGA</DataTitle>
+              <DataText>{formatDate(delivery.end_date)}</DataText>
+            </Data>
+          </DataRow>
+        </Card>
+        <CardAction>
+          <ActionButton onPress={() => {}}>
+            <Icon name="highlight-off" size={24} color="#E74040" />
+            <ActionText>Informar{'\n'}Problema</ActionText>
+          </ActionButton>
+          <ActionCenter onPress={() => {}}>
+            <ActionButton>
+              <Icon name="info-outline" size={24} color="#E7BA40" />
+              <ActionText>Visualizar{'\n'}Problemas</ActionText>
+            </ActionButton>
+          </ActionCenter>
+          <ActionButton onPress={() => {}}>
+            <Icon name="alarm-on" size={24} color="#7D40E7" />
+            <ActionText>Confirmar{'\n'}Entrega</ActionText>
+          </ActionButton>
+        </CardAction>
+      </Container>
+    </Background>
+  );
 }
+
+Details.navigationOptions = ({ navigation }) => ({
+  title: 'Detalhes da encomenda',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={25} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
+
+Details.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+    getParam: PropTypes.func.isRequired,
+  }).isRequired,
+};
--- mobile/src/pages/Delivery/Details/styles.js
+++ mobile/src/pages/Delivery/Details/styles.js
@@ -1,3 +1,81 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import { RectButton } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { BoxShadow } from '~/components/BoxShadow';
+
+export const Card = styled(BoxShadow)`
+  padding: 14px;
+  margin-bottom: 10px;
+`;
+
+export const CardHeader = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding-bottom: 8px;
+`;
+
+export const CardHeaderIcon = styled(Icon).attrs({
+  size: 20,
+  color: '#7d40e7',
+})``;
+
+export const CardHeaderTitle = styled.Text`
+  padding-left: 6px;
+  font-size: 14px;
+  font-weight: bold;
+  color: #7d40e7;
+`;
+
+export const Data = styled.View`
+  flex-direction: column;
+  padding-bottom: ${props => (props.last ? 0 : '16px')};
+`;
+
+export const DataRow = styled.View`
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-between;
+`;
+
+export const DataTitle = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #999;
+  margin-bottom: 6px;
+`;
+
+export const DataText = styled.Text`
+  font-size: 14px;
+  color: #666;
+`;
+
+export const CardAction = styled(BoxShadow)`
+  flex-direction: row;
+  background: #f8f9fd;
+  margin-bottom: 10px;
+`;
+
+export const ActionCenter = styled.View`
+  flex: 3;
+  border-left-width: 1px;
+  border-right-width: 1px;
+  border-left-color: #0000001a;
+  border-right-color: #0000001a;
+`;
+
+export const ActionButton = styled(RectButton)`
+  flex: 3;
+  flex-direction: column;
+  height: 84px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ActionText = styled.Text`
+  text-align: center;
+  margin-top: 2px;
+  font-size: 12px;
+  color: #999;
+`;
